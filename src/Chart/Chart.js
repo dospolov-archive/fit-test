ab9e5e8c04cf6b8fa464a8d76184cb8145f7d038
Marat Dospolov <marat.dospolov@gmail.com>
2020-10-27 06:04:28 +0200

    colors

--- src/Chart/Chart.js
+++ src/Chart/Chart.js
@@ -10,17 +10,17 @@ import React from 'react'
 const Message = ({ percent }) => {
   switch (true) {
     case percent < 25:
-      return 'Вы абсолютно несовестимы'
+      return 'Вы абсолютно несовестимы. Бегите.'
     case percent < 50:
-      return 'Скорее всего у вас нет будущего'
+      return 'Скорее всего у вас нет будущего, увы.'
     case percent < 62.5:
-      return 'Эти отношения могут стоить серьезных усилий, задумайтесь, нужно ли оно вам'
+      return 'Эти отношения могут стоить слишком серьезных усилий, задумайтесь, нужно ли оно вам.'
     case percent < 75:
-      return 'У вас может все получиться, если постараетесь'
+      return 'У вас может все получиться, если постараетесь!'
     case percent < 87.5:
-      return 'Вы отлично подходите друг другу, вам очень повезло'
-    case percent < 100:
-      return 'Ваш союз создан на небесах и вы идеально подходите друг другу'
+      return 'Вы отлично подходите друг другу, вам очень повезло!'
+    case percent <= 100:
+      return 'Ваш союз создан на небесах и вы идеально подходите друг другу!'
     default:
       return ''
   }
@@ -37,6 +37,13 @@ const Chart = ({ chart: { extremum, percent } }) => {
         <span className="chart-zero">0</span>
         <span className="chart-position" style={{ left: `${percent}%` }}></span>
         <span className="chart-max">{extremum}</span>
+
+        <i className="chart-bg-25"></i>
+        <i className="chart-bg-50"></i>
+        <i className="chart-bg-62"></i>
+        <i className="chart-bg-75"></i>
+        <i className="chart-bg-87"></i>
+        <i className="chart-bg-100"></i>
       </div>
     </div>
   )
